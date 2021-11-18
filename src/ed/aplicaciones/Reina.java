e4c7e77f45dd2e24616e18ff97a9c5d513fcdbbf
Eduardo <eduardo_lr@ciencias.unam.mx>
2021-11-17 19:02:49 -0600

    Clase Reina agregada

---
+++ src/ed/aplicaciones/Reina.java
@@ -0,0 +1,37 @@
+package ed.aplicaciones;
+
+public class Reina {
+
+	private int renglon;
+
+	private int columna;
+
+	public Reina(int renglon, int columna) {
+		this.renglon = renglon;
+		this.columna = columna;
+	}
+
+	public int getRenglon() {
+		return renglon;
+	}
+
+	public int getColumna() {
+		return columna;
+	}
+
+	public void mueveRenglon() {
+		renglon++;
+	}
+
+	public void mueveColumna() {
+		columna++;
+	}
+
+	public boolean esComida(Pila<Reina> pila) {
+		return false;
+	}
+
+	@override public boolean equals(Object object) {
+		return false;
+	}
+}
