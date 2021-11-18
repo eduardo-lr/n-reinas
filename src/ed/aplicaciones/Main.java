c2d5cb39f3317723c1ddfffc4638080c05db56ba
Eduardo <eduardo_lr@ciencias.unam.mx>
2021-11-17 18:02:33 -0600

    Firma del metodo ResuelveNReinas agregada

--- src/ed/aplicaciones/Main.java
+++ src/ed/aplicaciones/Main.java
@@ -4,12 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Pila<Integer> pila = new Pila<>();
-		pila.mete(5);
-		pila.mete(3);
-		pila.mete(8);
-		pila.mete(2);
-		for (int entero: pila)
-			System.out.println(entero);
     }
+
+	private static void ResuelveNReinas(int n) {
+
+	}
 }
