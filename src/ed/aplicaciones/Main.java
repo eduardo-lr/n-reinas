8483959b6ef411b2c3c8875c10a4334f1a385b00
Eduardo <eduardo_lr@ciencias.unam.mx>
2021-11-18 22:04:11 -0600

    Argumentos por linea de comandos admitido

--- src/ed/aplicaciones/Main.java
+++ src/ed/aplicaciones/Main.java
@@ -6,13 +6,29 @@ package ed.aplicaciones;
 public class Main {
 
     public static void main(String[] args) {
+	
+		if (!args[0].equals("${arg}")) {
+			try {
+				int i = Integer.parseInt(args[0]);
+				if (i <= 0)
+					throw new IllegalArgumentException();
+				Tablero tablero = new Tablero(i);
+				System.out.printf(tablero.resuelveNReinas());
+			} catch (NumberFormatException e) {
+				System.out.println("El argumento dado no es un número entero");
+				System.exit(1);
+			} catch (IllegalArgumentException e) {
+				System.out.println("El tamaño del tablero debe ser positivo");
+				System.exit(1);
+			}
+		} else {
+			/* Algunos casos de prueba. */
+			int[] casos = {1,2,3,5,8,23};
 
-		/* Algunos casos de prueba. */
-		int[] casos = {1,2,3,5,8,23};
-
-		for (int i : casos) {
-			Tablero tablero = new Tablero(i);
-			System.out.println(tablero.resuelveNReinas());
+			for (int i : casos) {
+				Tablero tablero = new Tablero(i);
+				System.out.println(tablero.resuelveNReinas());
+			}
 		}
     }
 }
